d0247d95798e5f436605c2c2d424cb40240ab0f1
Grant Moore <chinchalinchin@gmail.com>
2021-08-30 06:51:21 -0400

    update name

--- scrilla/core/asgi.py
+++ scrilla/core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pynance project.
+ASGI config for scrilla project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pynance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrilla.settings')
 
 application = get_asgi_application()
