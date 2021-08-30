e77b58889972220037c0c4666f244ec7537fb65c
Grant Moore <chinchalinchin@gmail.com>
2021-08-30 05:58:04 -0400

    wire analysis app into django project, add drf imports

--- pynance/core/settings.py
+++ pynance/core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'analysis.apps.AnalysisConfig'
 ]
 
 MIDDLEWARE = [
--- pynance/core/urls.py
+++ pynance/core/urls.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [ ]
+urlpatterns = [ 
+    path('analysis/', include('analysis.urls'))
+]
