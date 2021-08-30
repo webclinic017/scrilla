8e11c1803d3189f84268566de6aaef2211d32c65
Grant Moore <chinchalinchin@gmail.com>
2021-08-30 09:09:03 -0400

    fix typo, start scaffolding views

--- scrilla/api/views.py
+++ scrilla/api/views.py
@@ -4,6 +4,11 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def optimize(request):
     tickers = request.query_params.getlist('tickers', [])
-    
+    start_date = request.query_params.get('start_date')
+    end_date = request.query_params.get('end_date')
+    invest = request.query_params.get('invest')
+    target = request.query_params.get('target')
+    print(tickers)
+    print(start_date)
     pass
 
