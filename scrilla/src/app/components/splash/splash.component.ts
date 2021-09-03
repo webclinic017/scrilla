5f8c84455088fd43d1abdeeeea5c78545e8e6550
Grant Moore <chinchalinchin@gmail.com>
2021-09-03 06:17:25 -0400

    configuring splash scroller animations

--- scrilla/src/app/components/splash/splash.component.ts
+++ scrilla/src/app/components/splash/splash.component.ts
@@ -1,11 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { AnimationProperties, AnimationControl, AnimationService } from '../../services/animations.service';
 
 const scaleAnimationProperties : AnimationProperties = {
-  delay: '',
-  duration: '250ms',
-  easing: ''
+  delay: '', duration: '250ms', easing: ''
 }
+
+const translateAnimationProperties : AnimationProperties = {
+  delay: '', duration: '250ms', easing: ''
+}
+      // TODO: figure some way to pull hex color from theme palette instead of manually
+      // passing it into the animation...
+const highlightHexColor='#81C784'
+
 
 @Component({
   selector: 'app-splash',
@@ -13,20 +20,52 @@ const scaleAnimationProperties : AnimationProperties = {
   styleUrls: ['./splash.component.css'],
   animations: [
     AnimationService.getScaleTrigger(1.5, scaleAnimationProperties),
-      // TODO: figure some way to pull hex color from theme palette instead of manually
-      // passing it in the animation...
-    AnimationService.getHighlightTrigger('#81C784')
+    AnimationService.getHighlightTrigger(highlightHexColor),
+    AnimationService.getTranslateOffTrigger('100%', translateAnimationProperties),
+    AnimationService.getTranslateOnTrigger('100%', translateAnimationProperties)
   ]
 })
 export class SplashComponent implements OnInit {
 
-  public registerBtnAnimationControl: AnimationControl = this.animator.void();
-  public tryBtnAnimationControl : AnimationControl =  this.animator.void();
-  public rightNavAnimationControl: AnimationControl = this.animator.void();
-  public leftNavAnimationControl : AnimationControl = this.animator.void();
+  public paneIndex : number = 0;
+  public paneSize : number = 3;
+
+  public registerBtnAnimationControl: AnimationControl = this.animator.initAnimation();
+  public tryBtnAnimationControl : AnimationControl =  this.animator.initAnimation();
+  public rightNavAnimationControl: AnimationControl = this.animator.initAnimation();
+  public leftNavAnimationControl : AnimationControl = this.animator.initAnimation();
+
+  public paneAnimationControls : AnimationControl[]= [
+    this.animator.initAnimation(), this.animator.initAnimation(), this.animator.initAnimation()
+  ]
 
   constructor(public animator : AnimationService) { }
 
   ngOnInit(): void {}
   
+  public incrementPaneIndex(){
+    let previousIndex = this.paneIndex;
+    let bufferIndex = this.paneIndex
+    ++bufferIndex;
+    if(bufferIndex==this.paneSize){ bufferIndex = 0 }
+    this.paneAnimationControls[previousIndex] = this.animator.animateTranslateOffLeft();
+    let start = Date.now()
+    setTimeout(()=>{
+      console.log(Date.now() - start)
+      this.paneIndex=bufferIndex;
+      this.paneAnimationControls[this.paneIndex] = this.animator.animateTranslateOnRight();
+    }, 500)
+  }
+
+  public decrementPaneIndex(){
+    let previousIndex = this.paneIndex;
+    --this.paneIndex;
+    if(this.paneIndex==-1){ this.paneIndex = this.paneSize - 1}
+    //this.paneAnimationControls[previousIndex] = this.animator.animateTranslateOffRight();
+    //this.paneAnimationControls[this.paneIndex] = this.animator.animateTranslateOnLeft();
+  }
+
+  public animateButtons() : AnimationControl{
+    return {...this.animator.animateScale(), ...this.animator.animateHighlight()}
+  }
 }
--- scrilla/src/app/services/animations.service.ts
+++ scrilla/src/app/services/animations.service.ts
@@ -1,4 +1,4 @@
-import { animate, animateChild, keyframes, style, query, state, transition, trigger } from '@angular/animations';
+import { animate, animateChild, keyframes, style, query, state, transition, trigger, AnimationTriggerMetadata } from '@angular/animations';
 import { Injectable } from '@angular/core';
 
 export interface AnimationProperties{
@@ -6,13 +6,35 @@ export interface AnimationProperties{
 }
 
 export interface AnimationControl{
-    scale?: string, highlight?: string
+    scale?: string, highlight?: string, 
+    translateOff?: string, translateOn?: string
 }
 
-const scaleState = 'scale'
-const highlightState = 'highlight'
-const defaultAnimationProperties : AnimationProperties = {
+export const defaultAnimationProperties : AnimationProperties = {
     delay: '25ms', duration: '500ms', easing: ''
+}
+
+const scaleProperties = {
+    trigger: 'scale',
+    states: {
+        scale: 'scale'
+    }
+};
+const highlightProperties = {
+    trigger: 'highlight',
+    states: {
+        highlight: 'highlight'
+    }
+};
+const translateProperties = {
+    triggers: {
+        on: 'translateOn',
+        off: 'translateOff'
+    },
+    states: {
+        right: 'right',
+        left: 'left'
+    }
 }
 
 @Injectable({
@@ -21,16 +43,22 @@ const defaultAnimationProperties : AnimationProperties = {
 export class AnimationService{
 
     // SERVICE INSTANCE METHODS
-    public animate() : AnimationControl{
-        return { scale: `${scaleState}`, highlight: `${highlightState}` }
-    }
+    public animateScale() : AnimationControl{ return { scale: `${scaleProperties.states.scale}`} }
 
-    public void() : AnimationControl {
-        return { scale: '', highlight: ''}
-    }
+    public animateHighlight() : AnimationControl { return { highlight: `${highlightProperties.states.highlight}` }}
 
-    // STATIC METHODS
-    public static getAnimationString(properties : AnimationProperties){
+    public animateTranslateOffLeft() : AnimationControl { return { translateOff: `${translateProperties.states.left}` }}
+
+    public animateTranslateOffRight() : AnimationControl { return { translateOff: `${translateProperties.states.right}`}}
+
+    public animateTranslateOnLeft() : AnimationControl { return { translateOn: `${translateProperties.states.left}`}}
+
+    public animateTranslateOnRight() : AnimationControl { return { translateOn: `${translateProperties.states.right}`}}
+
+    public initAnimation() : AnimationControl { return { scale: '', highlight: '', translateOff: '', translateOn: ''} }
+
+    // STATIC FACTORY METHODS
+    public static getAnimationString(properties : AnimationProperties) : string{
         let animation_string = properties.duration;
         if(properties.delay){ animation_string = animation_string + ' ' + properties.delay}
         if(properties.easing){ animation_string = animation_string + ' ' + properties.easing}
@@ -38,18 +66,50 @@ export class AnimationService{
     }
 
     public static getScaleTrigger( scaleFactor: number,
-                                        properties : AnimationProperties = defaultAnimationProperties){
-        return trigger(`${scaleState}`, [
-            state(`${scaleState}`, style({ transform: `scale(${scaleFactor})`,})),
-            transition(`void <=> ${scaleState}`, [ animate(this.getAnimationString(properties))])
+                                        properties : AnimationProperties = defaultAnimationProperties) 
+    : AnimationTriggerMetadata{
+        return trigger(`${scaleProperties.trigger}`, [
+            state(`${scaleProperties.states.scale}`, style({ transform: `scale(${scaleFactor})`,})),
+            transition(`void <=> ${scaleProperties.states.scale}`, [ animate(this.getAnimationString(properties))])
         ]);
     }
 
     public static getHighlightTrigger(cssColor: string,
-                                        properties : AnimationProperties = defaultAnimationProperties){
-        return trigger(`${highlightState}`, [
-            state(`${highlightState}`, style({ backgroundColor: `${cssColor}`})),
-            transition(`void <=> ${highlightState}`, [ animate(this.getAnimationString(properties))])
+                                        properties : AnimationProperties = defaultAnimationProperties)
+    : AnimationTriggerMetadata{
+    
+        return trigger(`${highlightProperties.trigger}`, [
+            state(`${highlightProperties.states.highlight}`, style({ backgroundColor: `${cssColor}`})),
+            transition(`void <=> ${highlightProperties.states.highlight}`, [ animate(this.getAnimationString(properties))])
         ])                                   
+    }
+
+    public static getTranslateOffTrigger(distance: string, 
+                                            properties: AnimationProperties = defaultAnimationProperties) 
+    : AnimationTriggerMetadata {
+        return trigger(`${translateProperties.triggers.off}`, [
+            state(`${translateProperties.states.left}`, style({ transform: `translateX(-${distance})`, opacity: '0'})),
+            state(`${translateProperties.states.right}`, style({ transform: `translateX(${distance})`, opacity: '0'})),
+            transition('* => *', animate(this.getAnimationString(properties)))
+        ])
+    }
+
+    public static getTranslateOnTrigger(distance: string,
+                                            properties: AnimationProperties = defaultAnimationProperties)
+    : AnimationTriggerMetadata{
+        return trigger(`${translateProperties.triggers.on}`, [
+            transition(`* => ${translateProperties.states.left}`, [
+                animate(this.getAnimationString(properties), keyframes([
+                    style({ transform: `translateX(-${distance})`, opacity: '0', offset: 0}),
+                    style({ transform: 'translateX(0)', opacity: '1', offset: 1})
+                ]))
+            ]),
+            transition(`* => ${translateProperties.states.right}`,[
+                animate(this.getAnimationString(properties), keyframes([
+                    style({ transform: `translateX(${distance})`, opacity: '0', offset: 0}),
+                    style({ transform: 'translateX(0)', opacity: '1', offset: 1})
+                ]))
+            ])
+        ])
     }
 }
