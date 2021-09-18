5afdfe3daca0f381160ee98cea0af1a5737ba364
Grant Moore <chinchalinchin@gmail.com>
2021-09-18 10:10:39 -0400

    add help bar to banner, animate buttons for responsive design

--- scrilla/src/app/components/banner/banner.component.ts
+++ scrilla/src/app/components/banner/banner.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators'
+import { AnimationControl, AnimationService } from 'src/app/services/animations.service';
 import { StaticService } from 'src/app/services/static.service';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css'],
+  animations: [
+    AnimationService.getScaleTrigger(1.10),
+    AnimationService.getHighlightTrigger("#9E9E9E")
+  ]
 })
 
 export class BannerComponent implements OnInit {
@@ -16,9 +21,16 @@ export class BannerComponent implements OnInit {
 
   public menuState : string = 'closed';
 
+  public helpBar : boolean = false;
+
   public title !: string;
 
-  public constructor(private router: Router, private staticService: StaticService){
+  public whatBtnAnimationControl = this.animator.initAnimation();
+  public supportBtnAnimationControl = this.animator.initAnimation();
+  public featureBtnAnimationControl = this.animator.initAnimation();
+
+  public constructor(private router: Router, private staticService: StaticService,
+                      public animator: AnimationService){
     this.router.events
                     .pipe( 
                       filter( (data : any) => data instanceof NavigationEnd))
@@ -36,5 +48,13 @@ export class BannerComponent implements OnInit {
     if (this.menuState === 'closed'){ this.menuState = 'open'; }
     else { this.menuState = 'closed'}
     this.toggle.emit(this.menuState)
+  }
+
+  public toggleHelp(): void{
+    this.helpBar = !this.helpBar;
+  }
+
+  public animateButton(): AnimationControl{
+    return {...this.animator.animateScale(), ...this.animator.animateHighlight()}
   }
 }
